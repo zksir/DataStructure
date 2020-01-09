2213e8c58b2e1e90dd70a90ec89a4940f022064d
zhangkai <zksir@qq.com>
2020-01-09 21:56:25 +0800

    Dynamic
    动态数组接口设计

---
+++ 02-动态数组/src/com/zksir/ArrayList.java
@@ -0,0 +1,101 @@
+package com.zksir;
+
+public class ArrayList {
+
+    /**
+     * 元素的数量
+     */
+    private int size;
+
+    /**
+     * 所有元素
+     */
+    private int[] elements;
+
+    /**
+     * 清除所有元素
+     */
+    public void clear(){
+
+    }
+
+    /**
+     * 返回元素数量
+     * @return
+     */
+    public int size(){
+        return size;
+    }
+
+    /**
+     * 是否为空
+     * @return
+     */
+    public boolean isEmpty(){
+        return false;
+    }
+
+    /**
+     * 是否包含某个元素
+     * @param element
+     * @return
+     */
+    public boolean contains(int element){
+        return false;
+    }
+
+    /**
+     * 添加元素到尾部
+     * @param element
+     */
+    public void add(int element){
+
+    }
+
+    /**
+     * 获取index位置的元素
+     * @param index
+     * @return
+     */
+    public int get(int index){
+        return 0;
+    }
+
+    /**
+     * 设置index位置的元素
+     * @param index
+     * @param element
+     * @return
+     */
+    public int set(int index, int element){
+        return 0;
+    }
+
+    /**
+     * 在index位置插入一个元素
+     * @param index
+     * @param element
+     */
+    public void add(int index, int element){
+
+    }
+
+    /**
+     * 删除index位置的元素
+     * @param index
+     * @return
+     */
+    public int remove(int index) {
+        return 0;
+    }
+
+    /**
+     * 查看元素的索引
+     * @param element
+     * @return
+     */
+    public int indexOf(int element){
+        return 0;
+    }
+
+}
