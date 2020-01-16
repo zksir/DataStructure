0130d2fe5c680a0864c0f3d7b7b5fc779a50f6c8
zhangkai <zksir@qq.com>
2020-01-16 16:15:02 +0800

    141. 环形链表
    141. 环形链表

--- 00-leetcode/src/链表/_141_环形链表.java
+++ 00-leetcode/src/链表/_141_环形链表.java
@@ -1,5 +1,29 @@
 package 链表;
 
+/**
+ * https://leetcode-cn.com/problems/linked-list-cycle/
+ * @author kaizhang
+ *
+ */
 public class _141_环形链表 {
-
+	class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+    public boolean hasCycle(ListNode head) {
+    	if (head == null || head.next == null) return false;
+    	
+    	ListNode slow = head;
+    	ListNode fast = head.next;
+    	while (fast != null && fast.next != null) {
+			if (slow == fast) return true;
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+        return false;
+    }
 }
