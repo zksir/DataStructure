b692409cabc8a70ce8d6d15421012e02a853d06f
zhangkai <zksir@qq.com>
2020-05-16 10:02:43 +0800

    映射

---
+++ 10-映射/src/com/zksir/Main.java
@@ -0,0 +1,72 @@
+package com.zksir;
+
+import com.zksir.file.FileInfo;
+import com.zksir.file.Files;
+import com.zksir.map.Map;
+import com.zksir.map.TreeMap;
+import com.zksir.map.Map.Visitor;
+import com.zksir.set.Set;
+import com.zksir.set.TreeSet;
+
+public class Main {
+	
+	static void test1() {
+		Map<String, Integer> map = new TreeMap<>();
+		map.put("c", 2);
+		map.put("a", 5);
+		map.put("b", 6);
+		map.put("a", 8);
+		
+		map.traversal(new Visitor<String, Integer>() {
+			public boolean visit(String key, Integer value) {
+				System.out.println(key + "_" + value);
+				return false;
+			}
+		});
+	}
+	
+	static void test2() {
+		FileInfo fileInfo = Files.read("/Users/kaizhang/Desktop/src/java/util", 
+				new String[]{"java"});
+		
+		System.out.println("文件数量：" + fileInfo.getFiles());
+		System.out.println("代码行数：" + fileInfo.getLines());
+		String[] words = fileInfo.words();
+		System.out.println("单词数量：" + words.length);
+		
+		Map<String, Integer> map = new TreeMap<>();
+		for (int i = 0; i < words.length; i++) {
+			Integer count = map.get(words[i]);
+			count = (count == null) ? 1 : (count + 1);
+			map.put(words[i], count);
+		}
+		
+		map.traversal(new Visitor<String, Integer>() {
+			public boolean visit(String key, Integer value) {
+				System.out.println(key + "_" + value);
+				return false;
+			}
+		});
+	}
+	
+	static void test3() {
+		Set<String> set = new TreeSet<>();
+		set.add("c");
+		set.add("b");
+		set.add("c");
+		set.add("c");
+		set.add("a");
+		
+		set.traversal(new Set.Visitor<String>() {
+			public boolean visit(String element) {
+				System.out.println(element);
+				return false;
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+		test3();
+	}
+
+}
