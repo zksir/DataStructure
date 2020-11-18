bdaf7c3775cf0b1b65dc30c684358492d2bcf744
Scentea <zksir@qq.com>
2020-11-18 10:12:32 +0800

    1

--- 00-leetcode/src/链表/_237_删除链表中的节点.java
+++ 00-leetcode/src/链表/_237_删除链表中的节点.java
@@ -6,11 +6,6 @@ package 链表;
  *
  */
 public class _237_删除链表中的节点 {
-	public class ListNode {
-		int val;
-		ListNode next;
-		ListNode(int x) { val = x; }
-	}
 	
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
